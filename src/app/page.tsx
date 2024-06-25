c14b50420236e20345e829e1cff3f494bc301dd9
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2024-06-25 20:29:14 +0600

    change the updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
     fetch(`http://localhost:5000/g`, {
+      mode: "no-cors",
       method: "GET",
       credentials: "include",
     })
